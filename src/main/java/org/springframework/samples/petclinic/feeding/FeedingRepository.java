ad50537157caacd6a2516ca63011612699d84763
javib <javib@10.100.166.249>
2022-01-14 12:30:49 +0100

    Exámen completo

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
@@ -3,9 +3,20 @@ package org.springframework.samples.petclinic.feeding;
 import java.util.List;
 import java.util.Optional;
 
-public interface FeedingRepository {
-    List<Feeding> findAll();
-    // List<FeedingType> findAllFeedingTypes();
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FeedingRepository extends CrudRepository<Feeding, Integer>{
+	@Query("SELECT f FROM Feeding f")
+	List<Feeding> findAll();
+    
+    @Query("SELECT f FROM FeedingType f")
+    List<FeedingType> findAllFeedingTypes();
+    
+    @Query("SELECT f FROM FeedingType f WHERE f.name=?1")
+    FeedingType getFeedingType(String name);
+    
     Optional<Feeding> findById(int id);
-    Feeding save(Feeding p);
+    
+	Feeding save(Feeding p);
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
@@ -2,21 +2,28 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeedingService {
-    public List<Feeding> getAll(){
-        return null;
+	@Autowired 
+	private FeedingRepository feedingRepository;
+	
+	public List<Feeding> getAll(){
+        return feedingRepository.findAll();
     }
 
     public List<FeedingType> getAllFeedingTypes(){
-        return null;
+        return feedingRepository.findAllFeedingTypes();
     }
 
     public FeedingType getFeedingType(String typeName) {
-        return null;
+        return feedingRepository.getFeedingType(typeName);
     }
 
     public Feeding save(Feeding p) throws UnfeasibleFeedingException {
-        return null;       
+        return null;     
     }
 
     
